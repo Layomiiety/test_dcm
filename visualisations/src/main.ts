6ee77417932a0ec2d60cf2323acaecbb34eab989
Keyi Huang <kh747@cam.ac.uk>
2024-02-26 14:48:07 +0000

    multiple runs; can choose toggle on and off

--- visualisations/src/main.ts
+++ visualisations/src/main.ts
@@ -9,15 +9,16 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = 5;
 
 
-const trailLength = 20;
-const trailOpacity = 0.5;
+const trailLength = 30;
+const trailOpacity = 0.3;
 const particleSystems = [];
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8); // Set the size of the renderer to half the size of the window, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio * 4); // Increase renderer pixel ratio
 renderer.setClearColor(0xFFFFFF);
-document.body.appendChild(renderer.domElement);
+// document.body
+document.getElementById("test-div").appendChild(renderer.domElement);
 
 const gridSize = 20; // Size of the grid
 const gridDivisions = 10; // Number of divisions
@@ -27,51 +28,120 @@ const axesHelper = new THREE.AxesHelper(20); // Adjust the size of the axes as n
 scene.add(axesHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-// Enable damping for smooth camera movement
-controls.enableDamping = true;
+function control_setup(controls){
+  // Enable damping for smooth camera movement
+  controls.enableDamping = true;
 
-// Enable user interaction to rotate the camera around the scene
-controls.enableRotate = true;
+  // Enable user interaction to rotate the camera around the scene
+  controls.enableRotate = true;
 
-// Enable user interaction to zoom in and out
-controls.enableZoom = true;
+  // Enable user interaction to zoom in and out
+  controls.enableZoom = true;
 
-// Enable user interaction to pan the camera
-controls.enablePan = true;
+  // Enable user interaction to pan the camera
+  controls.enablePan = true;
 
-// Set the damping factor for camera rotation (higher value for slower rotation)
-controls.rotateSpeed = 0.5;
+  // Set the damping factor for camera rotation (higher value for slower rotation)
+  controls.rotateSpeed = 0.5;
 
-// Set the damping factor for camera zoom (higher value for slower zoom)
-controls.zoomSpeed = 0.5;
+  // Set the damping factor for camera zoom (higher value for slower zoom)
+  controls.zoomSpeed = 0.5;
 
-// Set the damping factor for camera panning (higher value for slower panning)
-controls.panSpeed = 0.5;
+  // Set the damping factor for camera panning (higher value for slower panning)
+  controls.panSpeed = 0.5;
 
-// Limit the range of vertical rotation (elevation angle)
-controls.minPolarAngle = 0; // in radians
-controls.maxPolarAngle = Math.PI; // in radians
+  // Limit the range of vertical rotation (elevation angle)
+  controls.minPolarAngle = -Math.PI; // in radians
+  controls.maxPolarAngle = Math.PI; // in radians
 
-// Limit the range of horizontal rotation
-controls.minAzimuthAngle = -Math.PI / 4; // in radians
-controls.maxAzimuthAngle = Math.PI / 4; // in radians
+  // Limit the range of horizontal rotation
+  controls.minAzimuthAngle = -Math.PI / 2; // in radians
+  controls.maxAzimuthAngle = Math.PI / 2; // in radians
 
-// Limit the range of zooming
-controls.minDistance = 1;
-controls.maxDistance = 100;
+  // Limit the range of zooming
+  controls.minDistance = 1;
+  controls.maxDistance = 100;
 
-// Set up the damping factor for camera movement
-controls.dampingFactor = 0.25;
+  // Set up the damping factor for camera movement
+  controls.dampingFactor = 0.25;
+}
+control_setup(controls);
+const geometry = new THREE.SphereGeometry(0.2, 32, 32);
 
-const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+const numFiles = 8;
 
+import * as data0 from './3body/0.00/deriv_generative.json';
+import * as data1 from './3body/0.01/deriv_generative.json';
+import * as data2 from './3body/0.02/deriv_generative.json';
+import * as data3 from './3body/0.03/deriv_generative.json';
+import * as data4 from './3body/0.04/deriv_generative.json';
+import * as data5 from './3body/0.05/deriv_generative.json';
+import * as data6 from './3body/0.06/deriv_generative.json';
+import * as data7 from './3body/0.07/deriv_generative.json';
+
+const jsonData = [data0, data1, data2, data3, data4, data5, data6, data7];
+
+const columnNames = Object.keys(jsonData[0]);
+
+console.log('names:' + columnNames);
+
+const data_length = jsonData[0].planet_coordsX_A.length;
+console.log('datalength:' + data_length);
 const materialColors = [
-  0xb71c1c, 0x1b5e20, 0x0d47a1, // True Materials
-  0xe53935, 0x43a047, 0x1e88e5, // Deriv Materials
-  0xe57373, 0x81c784, 0x64b5f6  // Org Materials
+  0xb71c1c, 0x1b5e20, 0x0d47a1,
+  0x8b0000, 0x006400, 0x00008b,
+  0xdc143c, 0x228b22, 0x4169e1,
+  0xff0000, 0x008000, 0x0000ff,
+  0xff6347, 0x32cd32, 0x1e90ff,
+  0xff6347, 0x00ff00, 0x00bfff,
+  0xff7f50, 0x7fff00, 0x87ceeb,
+  0xffa07a, 0xadff2f, 0xb0e0e6
 ];
 
+
+// // Function to generate a slightly different color from a given color
+// function generateSlightlyDifferentColor(color) {
+//   const r = (color >> 16) & 255;
+//   const g = (color >> 8) & 255;
+//   const b = color & 255;
+
+//   // Increase or decrease each component slightly
+//   const newR = r + Math.round(Math.random() * 20 - 10);
+//   const newG = g + Math.round(Math.random() * 20 - 10);
+//   const newB = b + Math.round(Math.random() * 20 - 10);
+
+//   // Ensure new values are within the valid range [0, 255]
+//   const clamp = (value) => Math.max(0, Math.min(255, value));
+
+//   return (clamp(newR) << 16) | (clamp(newG) << 8) | clamp(newB);
+// }
+
+// Generate colors for each group of paths
+// for (let i = 1; i < numFiles; i++) {
+//   const offset = i * 13000000  ;
+//   const baseColors = [0xb71c1c, 0x1b5e20, 0x0d47a1];
+//   const groupColors = baseColors.map(color => generateSlightlyDifferentColor(color));
+// materialColors.push(...groupColors);
+// }
+// console.log(materialColors);
+
+
 const spheres = []
+
+// Define a boolean flag to control animation pausing
+let isPaused = true;
+
+// Function to toggle animation pausing
+function togglePause() {
+  isPaused = !isPaused;
+}
+
+// Add event listener to the document to toggle pause on spacebar press
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space') {
+    togglePause();
+  }
+});
 
 function createSphere(materialIndex) {
   const material = new THREE.MeshBasicMaterial({color: materialColors[materialIndex]})
@@ -94,13 +164,13 @@ function createTrailParticleSystem(trailColor) {
   
   const particleMaterial = new THREE.PointsMaterial({
     color: trailColor,
-    size: 0.1,
+    size: 0.2,
     transparent: true,
     opacity: trailOpacity,
     blending: THREE.NormalBlending
   });
 
-  console.log("Trail Color:", trailColor); // Log the trail color
+  //console.log("Trail Color:", trailColor); // Log the trail color
 
 
   const particleSystem = new THREE.Points(particles, particleMaterial);
@@ -111,74 +181,78 @@ function createTrailParticleSystem(trailColor) {
 for (let i = 0; i < materialColors.length; i++) {
   createTrailParticleSystem(materialColors[i]);
 }
-// Load CSV data
-const csvFilePath1 = 'src/true_generative.csv'; 
-const csvFilePath2 = 'src/deriv_generative.csv'; 
-const csvFilePath3 = 'src/org_generative.csv'; 
 
-const coordinates = [];
+let currentIndex = 0;
+// Event listener to detect keypress events
+const spheres_on = [];
+for(let i = 0; i < numFiles * 3; i++) spheres_on.push(1);
+document.addEventListener('keypress', (event) => {
+  // Get the key pressed
+  const key = event.key;
 
-Promise.all([d3.text(csvFilePath1), d3.text(csvFilePath2), d3.text(csvFilePath3)]).then(([csvString1, csvString2, csvString3]) => {
-  const rows1 = csvString1.trim().split('\n');
-  const rows2 = csvString2.trim().split('\n');
-  const rows3 = csvString3.trim().split('\n');
-  
-  function generateCoordinates(rows, xIndex, yIndex, zIndex) {
-    return rows.map(row => {
-      const values = row.split(',');
-      return {
-        x: parseFloat(values[xIndex]),
-        y: parseFloat(values[yIndex]),
-        z: parseFloat(values[zIndex])
-      };
-    });
+  // Check if the pressed key is a number between 1 and 9
+  if (key >= '0' && key <= '7') {
+    // Calculate the index of the first sphere associated with the pressed key
+    const index = parseInt(key) * 3;
+
+    // Toggle the visibility of the spheres associated with the pressed key
+    for (let i = index; i < index + 3; i++) {
+      // Toggle visibility by setting the material's opacity
+      const sphere_on = spheres_on[i];
+      if(sphere_on == 1){
+        spheres_on[i] = 0;
+        scene.remove(spheres[i]);
+        scene.remove(particleSystems[i]);
+        console.log('removed sphere' + i);
+      }
+      else {
+        spheres_on[i] = 1;
+        scene.add(spheres[i]);
+        scene.add(particleSystems[i]);
+        console.log('added sphere' + i);
+      }
+    }
+    console.log("detected" + key);  
   }
-  coordinates.push(generateCoordinates(rows1, 1, 2, 3)); // true_coordinates_1
-  coordinates.push(generateCoordinates(rows1, 8, 9, 10)); // true_coordinates_2
-  coordinates.push(generateCoordinates(rows1, 15, 16, 17)); // true_coordinates_3
-  coordinates.push(generateCoordinates(rows2, 1, 2, 3)); // deriv_coordinates_1
-  coordinates.push(generateCoordinates(rows2, 8, 9, 10)); // deriv_coordinates_2
-  coordinates.push(generateCoordinates(rows2, 15, 16, 17)); // deriv_coordinates_3
-  coordinates.push(generateCoordinates(rows3, 1, 2, 3)); // org_coordinates_1
-  coordinates.push(generateCoordinates(rows3, 8, 9, 10)); // org_coordinates_2
-  coordinates.push(generateCoordinates(rows3, 15, 16, 17)); // org_coordinates_3
-  
-  let currentIndex = 0;
-
-  const animate = () => {
-    requestAnimationFrame(animate);
+});
+const animate = () => {
+  requestAnimationFrame(animate);
 
     // Move sphere towards the current coordinates
-    for (let i = 0; i < materialColors.length; i++) {
-      // Update the position of the ith sphere to match the current coordinates
-      spheres[i].position.x = coordinates[i][currentIndex].x;
-      spheres[i].position.y = coordinates[i][currentIndex].y;
-      spheres[i].position.z = coordinates[i][currentIndex].z;
+  if(!isPaused){
+      for (let i = 0; i < numFiles; i++) {
+        for(let k = 0 ; k < 3 ; k++){
+          // Update the position of the ith sphere to match the current coordinates
+          const added = ['A', 'B', 'C'];
+          spheres[3 * i + k].position.x = 3 * jsonData[i]['planet_coordsX_' + added[k]][currentIndex];
+          spheres[3 * i + k].position.y = 3 * jsonData[i]['planet_coordsY_' + added[k]][currentIndex];
+          spheres[3 * i + k].position.z = 3 * jsonData[i]['planet_coordsZ_' + added[k]][currentIndex];
+          // Update trail particles position associated with the ith sphere
+          const trailParticles = particleSystems[3 * i + k].geometry.attributes.position.array;
+          const spherePosition = spheres[3 * i + k].position;
+          
+          // Update position buffer attribute for the trail particles
+          for (let j = trailLength - 1; j >= 1; j--) { // Skip the first few particles
+            const index = j * 3;
+            const previousIndex = (j - 1) * 3;
 
-      // Update trail particles position associated with the ith sphere
-      const trailParticles = particleSystems[i].geometry.attributes.position.array;
-      const spherePosition = spheres[i].position;
-      
-      // Update position buffer attribute for the trail particles
-      for (let j = trailLength - 1; j >= 1; j--) { // Skip the first few particles
-        const index = j * 3;
-        const previousIndex = (j - 1) * 3;
+            // Shift the trail particles one step backward in the trail
+            trailParticles[index] = trailParticles[previousIndex];
+            trailParticles[index + 1] = trailParticles[previousIndex + 1];
+            trailParticles[index + 2] = trailParticles[previousIndex + 2];
+          }
 
-        // Shift the trail particles one step backward in the trail
-        trailParticles[index] = trailParticles[previousIndex];
-        trailParticles[index + 1] = trailParticles[previousIndex + 1];
-        trailParticles[index + 2] = trailParticles[previousIndex + 2];
+          // Update the position of the first particle to match the sphere
+          trailParticles[0] = spherePosition.x;
+          trailParticles[1] = spherePosition.y;
+          trailParticles[2] = spherePosition.z;
+
+          // Update particle buffer
+          particleSystems[3 * i + k].geometry.attributes.position.needsUpdate = true;
+        }
       }
-
-      // Update the position of the first particle to match the sphere
-      trailParticles[0] = spherePosition.x;
-      trailParticles[1] = spherePosition.y;
-      trailParticles[2] = spherePosition.z;
-
-      // Update particle buffer
-      particleSystems[i].geometry.attributes.position.needsUpdate = true;
+      currentIndex = (currentIndex + speed) % data_length;
     }
-  currentIndex = (currentIndex + speed) % coordinates[0].length;
   
   controls.update();
   // Render scene
@@ -186,5 +260,3 @@ Promise.all([d3.text(csvFilePath1), d3.text(csvFilePath2), d3.text(csvFilePath3)
   };
 
 animate();
-
-})
